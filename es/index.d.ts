3ee906c42ed9bd8ceae77060a9599611ae681fcd
rzh <peter.rao@derbysoft.net>
2025-01-17 14:10:28 +0800

    🔥 remove es/  && lib/

--- es/index.d.ts
+++
@@ -1,4 +0,0 @@
-import svg2Png from './applySvg2Png';
-import diffSvg from './applyDiffSvg';
-import removeEmptyCoordinates from './applyRemoveNanCoordinates';
-export { svg2Png, diffSvg, removeEmptyCoordinates, };
--- es/index.js
+++
@@ -1,5 +0,0 @@
-import svg2Png from './applySvg2Png';
-import diffSvg from './applyDiffSvg';
-import removeEmptyCoordinates from './applyRemoveNanCoordinates';
-export { svg2Png, diffSvg, removeEmptyCoordinates, };
-//# sourceMappingURL=index.js.map
