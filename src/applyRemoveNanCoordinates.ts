83a0df70423781440e4fd5f9dca9329eab3ee6db
pipi <rzh881004@icloud.com>
2025-01-24 14:57:30 +0800

    🚧 optimize attribute segmentation rules for svg path d (#9)
    Co-authored-by: rzh <peter.rao@derbysoft.net>

--- src/applyRemoveNanCoordinates.ts
+++ src/applyRemoveNanCoordinates.ts
@@ -17,24 +17,39 @@ export default function (svgContent: string): string {
   const dom = new JSDOM(svgContent, {
     contentType: 'image/svg+xml' // Set content type to SVG
   });
+  // Get the document and SVG element
   const document = dom.window.document;
+  // Get the SVG element
   const svgElement = document.querySelector('svg');
+
+  // Check if an SVG element was found
   if (!svgElement) {
     throw new Error('No SVG element found in the provided content.');
   }
+
   const paths = svgElement.querySelectorAll('path');
-
   // Iterate over each path element
-  Array.from(paths).forEach((path: any) => {
+  Array.from(paths).forEach((path) => {
     const d = path.getAttribute('d'); // Get the 'd' attribute
-    const commands = d.split(/(?=[MLHVCSQTAZ])/).map((command: string) => {
-      const type = command[0]; // Command type (e.g., M, L, H, etc.)
-      const params = command.slice(1).trim().split(/[\s,]+/).filter(Number); // Command parameters
-      return { type, params };
-    }).filter((command: { type: string, params: string[] }) => (command.type === 'Z' || command.params.length > 0)); // Filter out invalid commands
-
+    if (d === null) {
+      return;
+    }
+    // Split the 'd' attribute into commands and parameters
+    const commands = d
+      // Remove 'nan' and '-nan' values
+      .replace(/nan|-nan/g, ' ')
+      // Split the 'd' attribute into commands and parameters
+      .split(/(?=[MmLlHhVvCcSsQqTtAaZz])/).map((command) => {
+        // Split each command into type and parameters
+        const type = command[0];
+        // Split parameters by spaces or commas, filter out empty values, and convert to numbers
+        const params = command.slice(1).trim().split(/[\s,]+/).filter(Number);
+        return { type, params };
+      })
+      // Filter out commands with no parameters
+      .filter((command) => (command.type === 'Z' || command.params.length > 0));
     // Reconstruct the 'd' attribute
-    const modifiedD = commands.map((command: { type: string, params: string[] }) => {
+    const modifiedD = commands.map((command) => {
       return command.type + command.params.join(' ');
     }).join('');
     path.setAttribute('d', modifiedD); // Set the modified 'd' attribute
@@ -42,6 +57,5 @@ export default function (svgContent: string): string {
 
   // Get the new SVG content
   const newSvgContent = svgElement.outerHTML.trim();
-
   return newSvgContent;
 }
