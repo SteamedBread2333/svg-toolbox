b57af3154ef5f45a463c1de0e93e9949f5fb1536
pipi <rzh881004@icloud.com>
2025-02-14 12:33:40 +0800

    🔨 rename removeEmptyCoordinates -> removeNanCoordinates

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import svg2Png from './applySvg2Png';
 import diffSvg from './applyDiffSvg';
-import removeEmptyCoordinates from './applyRemoveNanCoordinates';
+import removeNanCoordinates from './applyRemoveNanCoordinates';
 import { createSVGElement, cloneSVGElement, mergeSVGElements, convertSVGToBase64, convertBase64ToSVG } from './utils'
+
+const removeEmptyCoordinates = removeNanCoordinates
 
 export {
   svg2Png,
   diffSvg,
+  /**
+   * @deprecated
+   * @see removeNanCoordinates
+   */
   removeEmptyCoordinates,
+  removeNanCoordinates,
   createSVGElement, cloneSVGElement, mergeSVGElements, convertSVGToBase64, convertBase64ToSVG
 };
