ce6baace1243b072a3e254e2fb4809b37fcfa115
pipi <rzh881004@icloud.com>
2025-02-14 14:37:52 +0800

    🚧 update applyRemoveNanCoordinates

--- src/applyRemoveNanCoordinates.ts
+++ src/applyRemoveNanCoordinates.ts
@@ -7,6 +7,9 @@
  */
 import { JSDOM } from 'jsdom';
 
+// Define the types of path commands that do not have parameters
+const ignoreTypes = ['z', 'Z'];
+
 /**
  * Parses and normalizes the 'd' attribute of all path elements in an SVG content.
  * @param {*} svgContent 
@@ -44,11 +47,13 @@ export default function (svgContent: string): string {
         // Split each command into type and parameters
         const type = command[0];
         // If the command is not 'z' or 'Z', then it has parameters.
-        if (type !== 'Z' && type !== 'z') {
+        const ignoreType = ignoreTypes.includes(type)
+        if (!ignoreType) {
           // Split parameters by spaces or commas, filter out empty values, and convert to numbers
           const params = command.slice(1).split(/[\s,]+/)
           // Filter out non-numeric values
-          if (params.every(Number)) {
+          const pickCommand = params.every(Number)
+          if (pickCommand) {
             return { type, params };
           }
         } else {
@@ -59,7 +64,7 @@ export default function (svgContent: string): string {
       .filter((item) => item !== void 0)
       // Filter out commands with no parameters, or commands with only 'z' or 'Z'. 'Zz' means close the path. 
       // -> https://developer.mozilla.org/zh-CN/docs/Web/SVG/Attribute/d#closepath
-      .filter((command) => (command.type === 'Z' || command.type === 'z' || command.params.length > 0));
+      .filter((command) => (ignoreTypes.includes(command.type) || command.params.length > 0));
     // Reconstruct the 'd' attribute
     const modifiedD = commands.map((command) => {
       return command.type + command.params.join(' ');
